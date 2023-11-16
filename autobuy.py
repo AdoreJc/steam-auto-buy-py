0c1bb3d84b096b596b2e7a1302f2bf755399463f
AdoreJC <45906467+AdoreJc@users.noreply.github.com>
2023-11-15 21:01:10 -0600

    Update autobuy.py
    添加excel订单记录

--- autobuy.py
+++ autobuy.py
@@ -1,8 +1,9 @@
+from datetime import datetime
+import pandas as pd
 from steampy.client import SteamClient
 from steampy.exceptions import ApiException, TooManyRequests
 from steampy.models import Currency, GameOptions
 import time
-from datetime import datetime
 
 # 设置Steam账户信息
 api_key = 'MY_API_KEY'
@@ -19,6 +20,9 @@ currency = Currency.CNY # 设置货币
 max_price = 12.34  # 设置最大购买价格
 max_quantity_to_buy = 1  # 设置最大购买数量
 quantity = 0 # 购买计数
+
+# 记录成功的订单列表
+successful_orders = []
 
 def login_to_steam():
     try:
@@ -42,7 +46,7 @@ while True:
 
         # 获取最低价格
         lowest_price = market_prices['lowest_price']
-        lowest_price = float(lowest_price[2:]) # 去掉货币字符
+        lowest_price = float(lowest_price[2:])
 
         # 当前时间
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -52,7 +56,7 @@ while True:
             quantity_to_buy = 1
 
             # 发起购买订单
-            price_string = str(int(lowest_price * 100))# 输入的价格是去掉小数点的字符 12.34 -> ‘1234’
+            price_string = str(int(lowest_price * 100))
             response = steam_client.market.create_buy_order(item_name, price_string, quantity_to_buy, GameOptions.CS,
                                                             currency=currency)
             buy_order_id = response["buy_orderid"]
@@ -61,6 +65,20 @@ while True:
             if buy_order_id:
                 print(f"{current_time} 成功购买 {quantity_to_buy} 个 {item_name} 在 ¥ {lowest_price} 。")
                 print(f"订单ID: {buy_order_id}")
+
+                # 记录成功的订单信息
+                successful_order_info = {
+                    "timestamp": current_time,
+                    "item_name": item_name,
+                    "quantity": quantity_to_buy,
+                    "price": lowest_price
+                }
+                successful_orders.append(successful_order_info)
+
+                # 保存成功的订单信息到 Excel 文件
+                df = pd.DataFrame(successful_orders)
+                df.to_excel("orders.xlsx", index=False)
+
                 quantity += 1
             else:
                 print(f"{current_time} 订单失败")
@@ -71,13 +89,18 @@ while True:
         # 大于购买数量停止
         if quantity >= max_quantity_to_buy:
             print(f"完成 {max_quantity_to_buy} 个订单采购")
+
+            # 打印成功的订单信息
+            print("成功的订单:")
+            print(successful_orders)
             break
 
-        time.sleep(3)
+        # 建议间隔大于等于3，不然API容易被限制，被限制之后关闭脚本一段时间后再使用即可
+        time.sleep(2)
 
     except TooManyRequests as e:
         print("TooManyRequests: 60s内最多请求20次")
-        time.sleep(60)
+        time.sleep(10)
 
     except Exception as e:
         print(f"Error: {e}")
